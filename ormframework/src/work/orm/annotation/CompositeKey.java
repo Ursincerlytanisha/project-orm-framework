7398191f8eb79ef2b88e6b407b42fb6721b17f05
Tanisha Chouhan <tchouhan855@gamil.com>
2026-09-09 12:40:03 +0530

    annotation

---
+++ ormframework/src/work/orm/annotation/CompositeKey.java
@@ -0,0 +1,8 @@
+package com.orm.annotation;
+import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CompositeKey
+{
+public String[] columns();
+}
